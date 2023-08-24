2cc6c8ec07315eb0e2042df6665b872369b4ecdc
estebanaltamura <esteban.altamura@gmail.com>
2023-08-24 14:08:45 -0300

    actualizacion

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -67,7 +67,7 @@ export const Footer = () => {
 
         <a
           className="footerContact  footerCV"
-          href="https://drive.google.com/file/d/1IUvNc5OnBI36W2_5_0A8plMlhCjXC9zz/view?usp=sharing"
+          href="https://drive.google.com/file/d/1z4UZE_jNp237KAX249oCa2WQnAEoZbqE/view?usp=sharing"
           target="_blank"
         >
           <FiFileText className="footerCVIcon" />
